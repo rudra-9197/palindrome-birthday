ea5459802847c44e72e821156d52fe87d12d5eb5
rudra-9197 <91685979+rudra-9197@users.noreply.github.com>
2021-12-09 20:50:50 +0530

    getNextDate

--- palindrome.js
+++ palindrome.js
@@ -66,13 +66,85 @@ for (var i=0; i< listOfPalindromes.length;i++){
 return flag;
     
 }
+function isLeapYear(year){
+if(year%400===0){
+    return true;
+}
+if(year%100===0){
+return true;
+}
+if (year%4 ==0){
+    return true;
+}
+return false;
+}
 
+function getNextDate(date){
 
+    var day = date.day + 1;
+    var month = date.month;
+    var year = date.year;
+
+    var daysInMonth = [31,28,31,30,31,30,31,31,30,31,30,31];
+
+    if(month===2){
+
+        if(isLeapYear(year)){
+            if(day>29){
+                day=1;
+                month++;
+            }
+        }
+            else{
+                if(day>28){
+                    day =1;
+                    month++;
+                }
+            }
+        }
+
+    
+    else {
+        if(day > daysInMonth[month-1]){
+            day = 1;
+            month++;
+
+        }
+    }
+if(month>12){
+    month=1;
+    year++;
+
+}
+return{
+    day: day,
+    month: month,
+    year: year
+}
+
+}
+
+function getNextPalindromeDate(date){
+
+    var ctr =0;
+    var nextDate = getNextDate(date);
+
+    while(1){
+ctr++;
+var isPalindrome = checkPalindromeForAllDateFormats(nextDate);
+if(isPalindrome){
+    break;
+}
+nextDate =getNextDate(nextDate);
+    }
+return [ctr, nextDate];
+}
 
 var date ={
-    day:2,
-    month: 11,
+    day: 31,
+    month: 12,
     year: 2020
-}
+};
+
+console.log(getNextPalindromeDate(date));
   
-console.log(checkPalindromeForAllDateFormats(date));
